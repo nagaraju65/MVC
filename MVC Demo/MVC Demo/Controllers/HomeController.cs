ccd2f1eabbdbaa4aa1aa1d0a3c03c038065eda11
Nagaraju Salendra <406255@cognizant.com>
2019-05-21 13:05:46 +0530

    Commit

---
+++ MVC Demo/MVC Demo/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Countries = new List<string>()
+                                {
+                                    "India",
+                                    "US",
+                                    "UK",
+                                    "Canada"
+                                };
+
+            return View();
+        }
+    }
+}
