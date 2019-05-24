3b36a44624bf681756720e7082b02196f5da15db
Nagaraju Salendra <406255@cognizant.com>
2019-05-24 15:25:02 +0530

    Models introduced

---
+++ MVC Demo/MVC Demo/Controllers/EmployeeController.cs
@@ -0,0 +1,19 @@
+﻿using MVC_Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Demo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Details()
+        {
+            Employee employee = new Employee() { Name = "Nagaraju", Age = 28, Gender = "Male" };
+            return View(employee);
+        }
+    }
+}
---
+++ MVC Demo/MVC Demo/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Demo.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+    }
+}
