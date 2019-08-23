e864d83701d6ae2f8217d247ba8307cc9eb4a24f
Nagaraju Salendra <406255@cognizant.com>
2019-08-23 16:11:51 +0530

    Html Helpers

---
+++ MVC Demo/MVC Demo 2/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Demo_2.Models;
+
+namespace MVC_Demo_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            Company company = new Company();
+            ViewBag.Departments = new SelectList(company.Departments, "DepartmentId", "DepartmentName");
+            return View(company);
+        }
+
+        [HttpPost]
+        [ActionName("Index")]
+        public string Index_post(Company company)
+        {
+            if (string.IsNullOrEmpty(company.SelectedDepartment))
+            {
+                return "you have not selected any department";
+            }
+            else
+            {
+                return "You have selected the department :" + company.SelectedDepartment;
+            }
+        }
+    }
+}
---
+++ MVC Demo/MVC Demo 2/Models/Company.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Demo_2.Models
+{
+    public class Company
+    {
+        [Key]
+        public string SelectedDepartment { get; set; }
+        public IEnumerable<Department> Departments
+        {
+            get
+            {
+                DBContext DB = new DBContext();
+                return DB.Departments.ToList();
+            }
+        }
+    }
+}
